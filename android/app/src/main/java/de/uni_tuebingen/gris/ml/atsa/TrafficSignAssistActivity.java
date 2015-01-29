c12231e825d7c6e7d49037c9fc6fc430ceea4efa
david <dvdgeisler@googlemail.com>
2015-01-29 17:14:45 +0100

    switch to api 14

--- android/app/src/main/java/de/uni_tuebingen/gris/ml/atsa/TrafficSignAssistActivity.java
+++ android/app/src/main/java/de/uni_tuebingen/gris/ml/atsa/TrafficSignAssistActivity.java
@@ -1,7 +1,7 @@
 package de.uni_tuebingen.gris.ml.atsa;
 
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
