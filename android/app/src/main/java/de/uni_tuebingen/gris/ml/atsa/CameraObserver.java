48fb56a97d3f268de0cb24155a124008ab9828a6
david <dvdgeisler@googlemail.com>
2015-01-31 13:22:23 +0100

    set to landscape mode

--- android/app/src/main/java/de/uni_tuebingen/gris/ml/atsa/CameraObserver.java
+++ android/app/src/main/java/de/uni_tuebingen/gris/ml/atsa/CameraObserver.java
@@ -7,6 +7,7 @@ import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
@@ -24,7 +25,6 @@ public class CameraObserver implements CameraBridgeViewBase.CvCameraViewListener
     private CVConvNet convNet;
     private String haarcascadePath;
     private String cnnPath;
-    private short[] lastResult = new short[] {0};
 
     public CameraObserver(String haarcascadePath, String cnnPath) {
         this.convNet = null;
@@ -45,6 +45,20 @@ public class CameraObserver implements CameraBridgeViewBase.CvCameraViewListener
 
     }
 
+    private void drawRect(Mat img, Rect rect, Scalar color) {
+        Point p1,p2,p3,p4;
+
+        p1 = new Point(rect.x,rect.y);
+        p2 = new Point(rect.x+rect.width,rect.y);
+        p3 = new Point(rect.x+rect.width,rect.y+rect.height);
+        p4 = new Point(rect.x,rect.y+rect.height);
+
+        Core.line(img,p1,p2,color,2);
+        Core.line(img,p2,p3,color,2);
+        Core.line(img,p3,p4,color,2);
+        Core.line(img,p4,p1,color,2);
+    }
+
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         short[] result;
@@ -52,31 +66,45 @@ public class CameraObserver implements CameraBridgeViewBase.CvCameraViewListener
         short x, y, w, h;
         Point p1,p2,p3,p4;
         Scalar color;
+        Mat subMat, subMatLeft, subMatRight;
+        Rect subMatLeftRect, subMatRightRect, resultRect;
 
         img = inputFrame.rgba();
-        result = this.convNet.perform(img);
-        color = new Scalar(255,0,0);
 
-        if(result[0] == 0)
-            result = this.lastResult;
-        else
-            this.lastResult = result;
+        subMatLeftRect = new Rect(0,0,img.width()/3,img.height());
+        subMatRightRect = new Rect((img.width()/3)*2,0,img.width()/3,img.height());
+        subMatLeft = img.submat(subMatLeftRect);
+        subMatRight = img.submat(subMatRightRect);
 
-        Log.i(TAG, String.format("found %d results " , result[0]));
+        result = this.convNet.perform(subMatLeft);
+        subMat = null;
         for(int i = 1; i < result.length; i+=5) {
-            x = result[i];
-            y = result[i+1];
+            x = (short) (result[i] + subMatLeftRect.x);
+            y = (short) (result[i+1] + subMatLeftRect.y);
             w = result[i+2];
             h = result[i+3];
-            p1 = new Point(x,y);
-            p2 = new Point(x+w,y);
-            p3 = new Point(x+w,y+h);
-            p4 = new Point(x,y+h);
-            Core.line(img,p1,p2,color,2);
-            Core.line(img,p2,p3,color,2);
-            Core.line(img,p3,p4,color,2);
-            Core.line(img,p4,p1,color,2);
+
+            resultRect = new Rect(new Point(x,y),new Point(x+w,y+h));
+            subMat = img.submat(resultRect);
+            this.drawRect(img,resultRect,new Scalar(255,0,0));
         }
+
+        result = this.convNet.perform(subMatRight);
+        subMat = null;
+        for(int i = 1; i < result.length; i+=5) {
+            x = (short) (result[i] + subMatRightRect.x);
+            y = (short) (result[i+1] + subMatRightRect.y);
+            w = result[i+2];
+            h = result[i+3];
+
+            resultRect = new Rect(new Point(x,y),new Point(x+w,y+h));
+            subMat = img.submat(resultRect);
+            this.drawRect(img,resultRect,new Scalar(255,0,0));
+        }
+
+
+        this.drawRect(img,subMatLeftRect,new Scalar(0,255,0));
+        this.drawRect(img,subMatRightRect,new Scalar(0,255,0));
 
         return img;
     }
